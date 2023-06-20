78aeda4347d8d60ce87d8337a9870001ecd204c7
Arpit Jana <arpitjana2103@gmail.com>
2023-06-20 18:05:25 +0530

    Learned Inline Functions

---
+++ cpp13_inlineFunctions.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+inline int product(int a, int b){
+    return a*b;
+}
+
+int main(){
+    int a = 10, b = 12;
+    cout << "Value of a is : " << a << endl;
+    cout << "Value of b is : " << b << endl;
+    cout << "Product of a & b is : " << product(a, b) << endl;
+    cout << "Product of a & b is : " << product(a, b) << endl;
+    cout << "Product of a & b is : " << product(a, b) << endl;
+    cout << "Product of a & b is : " << product(a, b) << endl;
+    cout << "Product of a & b is : " << product(a, b) << endl;
+    cout << "Product of a & b is : " << product(a, b) << endl;
+    return 0;
+}
