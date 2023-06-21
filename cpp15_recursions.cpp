4b1ab9e3931341791fd29be5eae5b78fbc142322
Arpit Jana <arpitjana2103@gmail.com>
2023-06-21 09:45:54 +0530

    Learned Recursion

--- cpp15_recursions.cpp
+++ cpp15_recursions.cpp
@@ -15,8 +15,37 @@ int factorial(int n)
 // factorial(5) = 5 * 4 * 3 * 2 * factoraial(1);
 // factorial(5) = 5 * 4 * 3 * 2 * 1
 
+int fibonacci(int n)
+{
+    if (n <= 1)
+    {
+        return 1;
+    }
+    return fibonacci(n - 2) + fibonacci(n - 1);
+}
+
+int fibonacci2(int n)
+{
+    if (n <= 1)
+        return 1;
+
+    int prev = 1;
+    int curr = 1;
+
+    while (n > 1)
+    {
+        int next = prev + curr;
+        prev = curr;
+        curr = next;
+        n--;
+    }
+    return curr;
+}
+
 int main()
 {
     cout << "The factorial of 5 is : " << factorial(5) << endl;
+    cout << fibonacci(3) << endl;
+    cout << fibonacci2(3) << endl;
     return 0;
 }
